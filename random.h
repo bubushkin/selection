4f3fe988d0b3ccd1aaa32d23952a5f1ab2f1ead7
bubushkin <bubushkin@users.noreply.github.com>
2018-11-08 12:14:37 -0500

    Create random.h

---
+++ random.h
@@ -0,0 +1,18 @@
+/*
+ * random.h
+ *
+ *  Created on: Nov 7, 2018
+ *      Author: iskandar
+ */
+
+#ifndef RANDOM_H_
+#define RANDOM_H_
+
+
+void shuffle(int *, int);
+
+int irandom();
+void gen_rand_vector(int *, int);
+
+
+#endif /* RANDOM_H_ */
